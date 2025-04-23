2a53a5cda0c2807e9471638c7e4defcf00600558
davidpinkevich <davidpinkevich95@gmail.com>
2025-04-23 15:48:55 +0300

    feat: add constants

---
+++ src/constants/index.ts
@@ -0,0 +1,12 @@
+import image1 from "../assets/images/1.webp";
+import image2 from "../assets/images/2.webp";
+import image3 from "../assets/images/3.webp";
+import image4 from "../assets/images/4.webp";
+import image5 from "../assets/images/5.webp";
+
+export const ROWS_ITEMS = [image1, image2, image3, image4, image5];
+
+export const TITILE_TEXT = "Simple Image Carousel";
+
+export const SUBTITILE_TEXT =
+  "5 x Image items ❖ Carousel Interactive Components & Variants";
